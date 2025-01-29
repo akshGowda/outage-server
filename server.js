fa766baf6025b97b2fc91f1255fa148b0223a352
akash <akash.n@nagra.com>
2025-01-29 15:41:37 +0530

    changes

--- server.js
+++ server.js
@@ -5,16 +5,23 @@ const port = 3000;
 
 // Simulate outage state
 let isOutage = false;
+let pollingInterval = 0;
+let Max = 0;
+
+app.use(express.json());
 
 // Endpoint to check outage status
 app.get('/outage', (req, res) => {
-  res.json({ outage: isOutage });
+  res.json({ outage: isOutage, pollingInterval: pollingInterval, Max: Max });
 });
 
 // Endpoint to toggle outage state for testing (optional for dev purposes)
 app.post('/toggle-outage', (req, res) => {
+  console.log(req.body);
   isOutage = !isOutage;
-  res.json({ message: `Outage state set to ${isOutage}` });
+  pollingInterval = req.body?.pollingInterval;
+  Max = req.body?.Max;
+  res.json({ message: `Outage state set to ${isOutage}, ${pollingInterval}, ${Max}` });
 });
 
 // Start the server
